7182851e2ebe9ef7b34fa5391deb243b8b01c097
Katie Lewis <33774231+kayjlou@users.noreply.github.com>
2025-11-20 11:16:05 -0800

    cmake updates

---
+++ CameraApp/build-test-script/CMakeFiles/CameraApp.dir/DependInfo.cmake
@@ -0,0 +1,68 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/ErrorHandling/ErrorHandler.cpp" "CMakeFiles/CameraApp.dir/src/ErrorHandling/ErrorHandler.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/ErrorHandling/ErrorHandler.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Fonts.cpp" "CMakeFiles/CameraApp.dir/src/Fonts.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Fonts.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/Camera/Camera.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/Camera/Camera.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/Camera/Camera.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/Camera/CameraRegisterAccess.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/Camera/CameraRegisterAccess.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/Camera/CameraRegisterAccess.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/Camera/WhiteBalance.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/Camera/WhiteBalance.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/Camera/WhiteBalance.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/CommunicationInterfaces/I2cCommunication.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/CommunicationInterfaces/I2cCommunication.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/CommunicationInterfaces/I2cCommunication.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/CommunicationInterfaces/UartCommunication.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/CommunicationInterfaces/UartCommunication.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/CommunicationInterfaces/UartCommunication.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/ControlBoard.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/ControlBoard.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/ControlBoard.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/Endoscope.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/Endoscope.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/Endoscope.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/GmslConnection.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/GmslConnection.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/GmslConnection.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/Light/LedControl.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/Light/LedControl.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/Light/LedControl.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/Temperature/ImxTemperature.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/Temperature/ImxTemperature.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/Temperature/ImxTemperature.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Hardware/Temperature/TemperatureMonitoring.cpp" "CMakeFiles/CameraApp.dir/src/Hardware/Temperature/TemperatureMonitoring.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Hardware/Temperature/TemperatureMonitoring.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Interaction/Action.cpp" "CMakeFiles/CameraApp.dir/src/Interaction/Action.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Interaction/Action.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Interaction/ActionHandler.cpp" "CMakeFiles/CameraApp.dir/src/Interaction/ActionHandler.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Interaction/ActionHandler.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Interaction/ButtonHandler.cpp" "CMakeFiles/CameraApp.dir/src/Interaction/ButtonHandler.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Interaction/ButtonHandler.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/MessageHandling/Message.cpp" "CMakeFiles/CameraApp.dir/src/MessageHandling/Message.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/MessageHandling/Message.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/MessageHandling/MessageHandler.cpp" "CMakeFiles/CameraApp.dir/src/MessageHandling/MessageHandler.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/MessageHandling/MessageHandler.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/OverlayRenderer.cpp" "CMakeFiles/CameraApp.dir/src/OverlayRenderer.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/OverlayRenderer.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/ActiveCameraOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/ActiveCameraOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/ActiveCameraOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/BrightnessOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/BrightnessOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/BrightnessOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/DebugOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/DebugOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/DebugOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/IconOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/IconOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/IconOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/ImageOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/ImageOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/ImageOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/MessageOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/MessageOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/MessageOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/RecordingModeOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/RecordingModeOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/RecordingModeOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/TextOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/TextOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/TextOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/TimedOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/TimedOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/TimedOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/TimerCircle.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/TimerCircle.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/TimerCircle.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Overlays/WhiteBalanceOverlay.cpp" "CMakeFiles/CameraApp.dir/src/Overlays/WhiteBalanceOverlay.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Overlays/WhiteBalanceOverlay.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Testing/ControlBoardLedControl.cpp" "CMakeFiles/CameraApp.dir/src/Testing/ControlBoardLedControl.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Testing/ControlBoardLedControl.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Testing/HandpieceLedControl.cpp" "CMakeFiles/CameraApp.dir/src/Testing/HandpieceLedControl.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Testing/HandpieceLedControl.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Testing/IntegrationTesting.cpp" "CMakeFiles/CameraApp.dir/src/Testing/IntegrationTesting.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Testing/IntegrationTesting.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Utils/FileUtils.cpp" "CMakeFiles/CameraApp.dir/src/Utils/FileUtils.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Utils/FileUtils.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Utils/Utils.cpp" "CMakeFiles/CameraApp.dir/src/Utils/Utils.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Utils/Utils.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Utils/WindowUtils.cpp" "CMakeFiles/CameraApp.dir/src/Utils/WindowUtils.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Utils/WindowUtils.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Versions/Versions.cpp" "CMakeFiles/CameraApp.dir/src/Versions/Versions.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Versions/Versions.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Video.cpp" "CMakeFiles/CameraApp.dir/src/Video.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Video.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/Window.cpp" "CMakeFiles/CameraApp.dir/src/Window.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/Window.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/src/main.cpp" "CMakeFiles/CameraApp.dir/src/main.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/src/main.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/third_party/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/CameraApp.dir/third_party/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/third_party/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/third_party/imgui/backends/imgui_impl_sdl2.cpp" "CMakeFiles/CameraApp.dir/third_party/imgui/backends/imgui_impl_sdl2.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/third_party/imgui/backends/imgui_impl_sdl2.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/third_party/imgui/imgui.cpp" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/third_party/imgui/imgui_draw.cpp" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui_draw.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/third_party/imgui/imgui_tables.cpp" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui_tables.cpp.o.d"
+  "/Users/katielewis/Desktop/Linux_camera/CameraApp/third_party/imgui/imgui_widgets.cpp" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/CameraApp.dir/third_party/imgui/imgui_widgets.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
